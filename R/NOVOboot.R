62ace3540e6b5c48140745a873a0ddd7de8faca3
Rhodes <nrhode@midwestern.edu>
2021-02-27 14:57:42 -0600

    Signed-off-by: Rhodes <nrhode@midwestern.edu>

--- R/NOVOboot.R
+++ R/NOVOboot.R
@@ -20,6 +20,8 @@
 #' @return An array of percentiles from 0\% to 100\% capturing the resampled
 #'   model and chance performance metrics. The simulated bootstraps are stored
 #'   within the object \code{novo.boot.x} which is appended with the current
+#'   \code{run} number \code{x}. The model and chance quantile summary is stored
+#'   within the object \code{sum.boot.x} which is appended with the current
 #'   \code{run} number \code{x}.
 #'
 #' @details The first axiom of novometric theory states that, for a random
@@ -31,15 +33,30 @@
 #'   effect strength of the model is statistically significant at the confidence
 #'   level selected by the user.
 #'
-#'   \code{NOVOboot} reports the distribution of ESS in quantiles from 0\% to
-#'   100\%. This provides a mechanism to evaluate the validity of an ODA model
-#'   through bootstrap resampling with replacement. The same methodology can be
-#'   applied to any classifcation model wherein a two class and two attribute
-#'   confusion matrix can be formulated. It is axiomatic that the exact discrete
-#'   confidence intervals for model and chance define the boundry between
-#'   "signal" and "noise". A distribution of p values from Fisher's Exact tests
-#'   conducted for each bootstrap replicate are also supplied and can be
-#'   graphically evaluated, as in the example below.
+#'   \code{NOVOboot} reports the Effect Strength for Sensitivity (ESS), which is
+#'   the mean Percent Accuracy in Classification (mean PAC) corrected for
+#'   chance, in quantiles from 0\% to 100\% in the first slot of the
+#'   \code{sum.boot.x}. For a 2 x 2 matrix, mean PAC is equivalent to the ROC
+#'   area. \code{NOVOboot} also reports the distribution of mean PAC in
+#'   quantiles from 0\% to 100\% in the second slot of \code{sum.boot.x}.
+#'   Because a binary classification matrix can also expressed as an odds ratio
+#'   (OR) or as a risk ratio (RR), \code{NOVOboot} also reports estimates of the
+#'   OR and RR for the model and chance effect distributions in the third and
+#'   fourth slots of the \code{sum.boot.x} object, respectively. Likewise, one
+#'   may wish to consider the corresponding distribution of p values from
+#'   Fisher's Exact tests conducted for each bootstrap replicate. These
+#'   distributions for both model and chance are reported in the fifth slot of
+#'   the \code{sum.boot.x} object.
+#'
+#'   The results of this non-parametric bootstrap analysis provide a mechanism
+#'   to evaluate the precision of point estimate of an effect provided by an ODA
+#'   model. The same methodology can be applied to any classifcation model
+#'   wherein a two class and two attribute confusion matrix can be formulated.
+#'   It is axiomatic that the exact discrete confidence intervals for model and
+#'   chance define the boundry between "signal" and "noise". For effects wherein
+#'   the distributions of model and chance overlap, it is concluded that the
+#'   effect was not significantly different vs. the distribution of chance given
+#'   the data.
 #'
 #' @export
 #'
@@ -51,9 +68,9 @@
 #'
 #' @examples
 #' ## Not run
-#' ## NOVOboot(data=oda.list.1,run=1,predictor=8,outcome=1,seed=1234)
+#' ## NOVOboot(data=oda.list.1,run=1,predictor=1,outcome=1,seed=1234)
 #'
-#' ## Example of a moderate effect size (ESS) Novometric bootstrap confidence interval
+#' ## Example of a moderate effect size (ESS) exact discrete 95% confidence interval
 #' ess <- ((((14/14)+(20/64))/2)-0.5)/.5        # 31.25% ESS, a moderate effect
 #' data <- matrix(c(20,0,44,14),ncol=2,nrow=2,dimnames=list(c(0,1),c(0,1)))
 #' data.raw <- epitools::expand.table(data)
@@ -62,7 +79,27 @@
 #' oda.list.1[[1]] <- do.call("list",data.tab)  # supply data for NOVOboot
 #'
 #' # NOVOboot(data=oda.list.1,run=1,predictor=1,outcome=1,seed=1234, nboot=25000)
-#' # hist(novo.boot.1$p, main="Distribution of P-values \n by Fisher's Exact Test")
+#'
+#' # boot.1 <- novo.boot.1 # added after NOVOboot() run
+#'
+#' # print(sum.boot.1[[1]]) # displays the quantile summary of Model vs. Chance ESS
+#'
+#' # boot.list <- setNames(data.frame(boot.1$ess.model,boot.1$ess.chance),c("Model","Chance"))
+#'
+#' # df <- stack(boot.list,select=c("Model","Chance"))
+#'
+#' # library(ggplot2)
+#' # ggplot(df,aes(x=values)) +
+#' # geom_histogram(data=subset(df, ind== 'Chance'),fill="skyblue",colour="black",binwidth = 2) +
+#' # geom_histogram(data=subset(df, ind== 'Model'), fill="pink", colour="black",binwidth=2) +
+#' # xlab("Effect strength for sensitivity (ESS %)") +
+#' # ylab("Frequency of bootstrap replicates") +
+#' # geom_vline(aes(xintercept = quantile(boot.1$ess.model,probs=0.025, na.rm=T), color="LB"), linetype="dashed") +
+#' # geom_vline(aes(xintercept = quantile(boot.1$ess.chance,probs=0.975, na.rm=T), color="UB"), linetype="dashed") +
+#' # labs(title = "Bootstrap 95% Interval for ESS from Final Model vs. 95% Interval for Chance",
+#' # subtitle = "ESS Distribution for Chance (Blue) vs. Model (Red) resamples (n=25,000)") +
+#' # scale_color_manual(name = "95% PI", values = c(UB = "Blue", LB = "Red")) +
+#' # theme_bw()
 #'
 #' @references Yarnold, P.R. (2020). Reformulating the First Axiom of Novometric
 #'   Theory: Assessing Minimum Sample Size in Experimental Desig \emph{Optimal
@@ -119,19 +156,23 @@ NOVOboot <- function(data="",run="",predictor="",outcome="",nboot="",seed=""){
   mpac <- list()
   ess  <- list()
   p    <- list()
+  or   <- list()
+  rr   <- list()
   for(i in 1:reps) {
     sens[[i]] <- tab[[i]][2,2]/sum(tab[[i]][2,2],tab[[i]][2,1])
     spec[[i]] <- tab[[i]][1,1]/sum(tab[[i]][1,1],tab[[i]][1,2])
     mpac[[i]] <- (sens[[i]]+spec[[i]])/2
     ess[[i]]  <- (mpac[[i]]-0.5)/0.5
     p[[i]]    <- fisher.test(tab[[i]])[1]
+    or[[i]]   <- (tab[[i]][2,2] / tab[[i]][1,2]) / (tab[[i]][2,1] / tab[[i]][1,1])
+    rr[[i]]   <- (tab[[i]][2,2] / sum(tab[[i]][2,2],tab[[i]][1,2])) / (tab[[i]][2,1] / sum(tab[[i]][2,1],tab[[i]][1,1]))
   }
 
-  metrics  <- data.frame(cbind(sens,spec,mpac,ess,p))
-  result   <- as.data.frame(sapply(metrics, unlist))*100
-  result$p <- result$p/100
-  chance   <- round(result[,-5], digits=2)
-  chance$p <- result$p
+  metrics <- data.frame(cbind(sens,spec,mpac,ess,p,or,rr))
+  result  <- as.data.frame(sapply(metrics, unlist))
+  result[1:4]  <- round(result[1:4]*100,digits=2)
+  result[6:7]  <- round(result[6:7], digits=3)
+  chance  <- result
 
   # Model bootstap for 95% CI for model
   dat   <- list()
@@ -151,23 +192,62 @@ NOVOboot <- function(data="",run="",predictor="",outcome="",nboot="",seed=""){
   mpac <- list()
   ess  <- list()
   p    <- list()
+  or   <- list()
+  rr   <- list()
   for(i in 1:reps) {
     sens[[i]] <- tab[[i]][2,2]/sum(tab[[i]][2,2],tab[[i]][2,1])
     spec[[i]] <- tab[[i]][1,1]/sum(tab[[i]][1,1],tab[[i]][1,2])
     mpac[[i]] <- (sens[[i]]+spec[[i]])/2
     ess[[i]]  <- (mpac[[i]]-0.5)/0.5
     p[[i]]    <- fisher.test(tab[[i]])[1]
+    or[[i]]   <- (tab[[i]][2,2] / tab[[i]][1,2]) / (tab[[i]][2,1] / tab[[i]][1,1])
+    rr[[i]]   <- (tab[[i]][2,2] / sum(tab[[i]][2,2],tab[[i]][1,2])) / (tab[[i]][2,1] / sum(tab[[i]][2,1],tab[[i]][1,1]))
   }
 
-  metrics  <- data.frame(cbind(sens,spec,mpac,ess,p))
-  result   <- as.data.frame(sapply(metrics, unlist))*100
-  result$p <- result$p/100
-  model    <- round(result[,-5], digits=2)
-  model$p  <- result$p
+  metrics <- data.frame(cbind(sens,spec,mpac,ess,p,or,rr))
+  result  <- as.data.frame(sapply(metrics, unlist))
+  result[1:4]  <- round(result[1:4]*100,digits=2)
+  result[6:7]  <- round(result[6:7], digits=3)
+  model   <- result
 
-  boot.out <- setNames(data.frame(model,chance),paste0(rep(c("sens.","spec.","mpac.","ess.","p."),times=2),rep(c("model","chance"),each=5)))
+  boot.out <- setNames(data.frame(model,chance),paste0(rep(c("sens.","spec.","mpac.","ess.","p.","or.","rr."),times=2),rep(c("model","chance"),each=7)))
 
-  out <- setNames(data.frame(quantile(model$ess, probs=c(0,0.025,0.05,0.25,0.5,0.75,0.95,0.975,1),na.rm=T),quantile(chance$ess, probs=c(0,0.025,0.05,0.25,0.5,0.75,0.95,0.975,1),na.rm=T)),c("ESS(%) Model","ESS(%) Chance"))
+  out <- setNames(data.frame(
+    quantile(
+      model$ess, probs=c(0,0.025,0.05,0.25,0.5,0.75,0.95,0.975,1),na.rm=T),
+    quantile(
+      chance$ess, probs=c(0,0.025,0.05,0.25,0.5,0.75,0.95,0.975,1),na.rm=T),
+    quantile(
+      model$mpac, probs=c(0,0.025,0.05,0.25,0.5,0.75,0.95,0.975,1),na.rm=T),
+    quantile(
+      chance$mpac, probs=c(0,0.025,0.05,0.25,0.5,0.75,0.95,0.975,1),na.rm=T),
+    quantile(
+      model$or, probs=c(0,0.025,0.05,0.25,0.5,0.75,0.95,0.975,1),na.rm=T),
+    quantile(
+      chance$or, probs=c(0,0.025,0.05,0.25,0.5,0.75,0.95,0.975,1),na.rm=T),
+    quantile(
+      model$rr, probs=c(0,0.025,0.05,0.25,0.5,0.75,0.95,0.975,1),na.rm=T),
+    quantile(
+      chance$rr, probs=c(0,0.025,0.05,0.25,0.5,0.75,0.95,0.975,1),na.rm=T)
+    quantile(
+      model$p, probs=c(0,0.025,0.05,0.25,0.5,0.75,0.95,0.975,1),na.rm=T),
+    quantile(
+      chance$p, probs=c(0,0.025,0.05,0.25,0.5,0.75,0.95,0.975,1),na.rm=T)),
+    c("ESS(%) Model","ESS(%) Chance","MPAC(%) Model","MPAC(%) Chance","OR Model","OR Chance","RR Model","RR Chance","P-value Model","P-value Chance"))
+
+  boot <- list()
+
+  boot[[1]] <- out[1:2]
+
+  boot[[2]] <- out[3:4]
+
+  boot[[3]] <- out[5:6]
+
+  boot[[4]] <- out[7:8]
+
+  boot[[5]] <- out[9:10]
+
   assign(paste0("novo.boot.",run), boot.out, pos = parent.frame())
-  return(out)
+  assign(paste0("sum.boot.",run), boot, pos = parent.frame())
+  return(boot[[1]])
 }
